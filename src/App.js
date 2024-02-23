f1c1e0199a4a25083fcf95fa0c01f7b6919bab97
Rajan Singh <imraj20@outlook.com>
2024-02-23 18:19:14 +0530

    website uploaded

---
+++ src/App.js
@@ -0,0 +1,330 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <meta charSet="utf-8" />
+    <link rel="icon" href="/favicon.ico" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="theme-color" content="#000000" />
+    <title>Frame</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter%3A400%2C700" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro%3A400%2C700" />
+    <link rel="stylesheet" href="./styles/frame.css" />
+    <div className="frame-yrf">
+      <div className="auto-group-pjmd-N87">
+        <div className="image-X2w">
+        </div>
+        <div className="rectangle-dLs">
+        </div>
+        <img className="image-V8B" src="./assets/image.png" />
+        <p className="categories-Ak7">Categories</p>
+        <p className="website-builders-qLT">Website Builders</p>
+        <p className="todays-deals-tZd">Today's deals</p>
+      </div>
+      <div className="auto-group-629m-mdR">
+        <div className="rectangle-TPm">
+        </div>
+        <div className="rectangle-mvF">
+        </div>
+        <div className="rectangle-Kgs">
+        </div>
+        <div className="rectangle-48f">
+        </div>
+        <div className="item--9fu">
+          <img className="vector-cJb" src="./assets/vector-iLf.png" />
+          <img className="vector-uHh" src="./assets/vector-KiP.png" />
+          <img className="vector-BW7" src="./assets/vector-S7Z.png" />
+          <img className="vector-LP1" src="./assets/vector-tas.png" />
+          <img className="vector-Hp3" src="./assets/vector-hKd.png" />
+        </div>
+        <div className="rectangle-zCf">
+        </div>
+        <p className="builder-1-KVq">Builder 1</p>
+        <p className="wixpro-72-inch-responsive-website-builder-comprehensive-digital-platform-creation-tool-streamlined-design-interface-for-professional-websites-and-online-stores-black-blue-S4f">
+          <span className="wixpro-72-inch-responsive-website-builder-comprehensive-digital-platform-creation-tool-streamlined-design-interface-for-professional-websites-and-online-stores-black-blue-S4f-sub-0">WixPro 72-Inch Responsive Website Builder-</span>
+          <span className="wixpro-72-inch-responsive-website-builder-comprehensive-digital-platform-creation-tool-streamlined-design-interface-for-professional-websites-and-online-stores-black-blue-S4f-sub-1">Comprehensive Digital Platform Creation Tool, Streamlined Design Interface for Professional Websites and Online Stores (Black/Blue)</span>
+        </p>
+        <p className="main-highlights-Qhd">Main highlights</p>
+        <p className="what-you-get-receive-the-wixpro-website-builder-suite-access-to-premium-design-templates-an-extensive-library-of-widgets-and-apps-and-detailed-step-by-step-guides-hwd">[What You Get]: Receive the WixPro website builder suite, access to premium design templates, an extensive library of widgets and apps, and detailed step-by-step guides.</p>
+        <img className="download-6-ZU3" src="./assets/download-6.png" />
+        <p className="show-more-Huq">Show more</p>
+        <p className="item-93-mKD">9.3</p>
+        <p className="exceptional-qps">Exceptional</p>
+        <p className="view-mCj">View</p>
+        <div className="rectangle-fZ1">
+        </div>
+        <p className="item-3-kqM">3</p>
+        <div className="rectangle-5cj">
+        </div>
+        <div className="rectangle-b5H">
+        </div>
+        <div className="rectangle-jSP">
+        </div>
+        <div className="rectangle-fqq">
+        </div>
+        <p className="off-R4K">20% Off</p>
+        <p className="limited-time-ukB">Limited time </p>
+        <p className="webbuilder-1-Y2T">Webbuilder 1</p>
+        <p className="item-3996-dpb">$39.96</p>
+        <p className="item-4996-A3q">$49.96</p>
+        <p className="off-UqD">(20% Off)</p>
+        <p className="view-deal-QTy">View Deal</p>
+        <p className="computer-modern-clasic-with-wix-support-Uyd">Computer  Modern clasic with wix support</p>
+        <div className="rectangle-CPq">
+        </div>
+        <div className="rectangle-i7H">
+        </div>
+        <div className="rectangle-rjH">
+        </div>
+        <div className="rectangle-QVu">
+        </div>
+        <img className="image-MAF" src="./assets/image-Vzo.png" />
+        <img className="image-gyD" src="./assets/image-d6X.png" />
+        <div className="rectangle-Buy">
+        </div>
+        <img className="image-jAo" src="./assets/image-rmu.png" />
+        <p className="cdk-F99">CDK</p>
+        <div className="rectangle-7x3">
+        </div>
+        <div className="rectangle-Tm1">
+        </div>
+        <div className="rectangle-Cib">
+        </div>
+        <div className="rectangle-86T">
+        </div>
+        <div className="rectangle-cGX">
+        </div>
+        <div className="rectangle-kNj">
+        </div>
+        <div className="rectangle-siF">
+        </div>
+        <div className="rectangle-Byq">
+        </div>
+        <img className="image-J2s" src="./assets/image-zJT.png" />
+        <div className="rectangle-pX1">
+        </div>
+        <img className="image-xdD" src="./assets/image-YWB.png" />
+        <div className="rectangle-tmm">
+        </div>
+        <img className="image-cxf" src="./assets/image-E8F.png" />
+        <div className="image-AUP">
+          <img className="vector-fg3" src="./assets/vector-wiX.png" />
+          <img className="vector-1ju" src="./assets/vector-Lo9.png" />
+          <img className="vector-NKZ" src="./assets/vector-ALf.png" />
+          <img className="vector-iuD" src="./assets/vector-CAj.png" />
+          <img className="vector-Ecf" src="./assets/vector-qCT.png" />
+        </div>
+        <div className="rectangle-PkT">
+        </div>
+        <div className="rectangle-6Py">
+        </div>
+        <div className="rectangle-deo">
+        </div>
+        <img className="image-Lp7" src="./assets/image-vWB.png" />
+        <div className="rectangle-GBy">
+        </div>
+        <div className="rectangle-ukj">
+        </div>
+        <img className="image-o5R" src="./assets/image-Vmq.png" />
+        <div className="rectangle-5od">
+        </div>
+        <img className="image-zvb" src="./assets/image-3KM.png" />
+        <div className="image-81D">
+          <img className="vector-Fbd" src="./assets/vector-dvF.png" />
+          <img className="vector-bvP" src="./assets/vector-8rT.png" />
+          <img className="vector-X3M" src="./assets/vector-R6P.png" />
+          <img className="vector-pYF" src="./assets/vector-CGw.png" />
+          <img className="vector-yg3" src="./assets/vector-DGB.png" />
+        </div>
+        <div className="rectangle-CHu">
+        </div>
+        <img className="image-vzb" src="./assets/image-RkB.png" />
+        <div className="image-3ZR">
+        </div>
+        <img className="image-Ae3" src="./assets/image-2X1.png" />
+        <div className="image-HTm">
+        </div>
+        <img className="image-pTh" src="./assets/image-oeo.png" />
+        <div className="image-7hh">
+        </div>
+        <img className="image-4N3" src="./assets/image-bYb.png" />
+        <div className="image-Qgo">
+        </div>
+        <img className="image-Wzj" src="./assets/image-SuD.png" />
+        <div className="image-TQB">
+        </div>
+        <div className="rectangle-PYj">
+        </div>
+        <div className="rectangle-uX5">
+        </div>
+        <div className="rectangle-3tB">
+        </div>
+        <div className="rectangle-zHd">
+        </div>
+        <div className="rectangle-vh5">
+        </div>
+        <div className="image-rqd">
+        </div>
+        <img className="image-QcF" src="./assets/image-NKq.png" />
+        <p className="best-website-builders-in-the-us-7Fm">Best Website builders in the US</p>
+        <p className="advertising-disclosure-ouH">Advertising Disclosure</p>
+        <p className="last-updated-Di7">Last Updated</p>
+        <p className="item--ies">-</p>
+        <p className="february-22-2020-S55">February 22, 2020</p>
+        <p className="top-relevant-YP1">Top Relevant</p>
+        <p className="tools-opj">Tools</p>
+        <p className="aws-builder-8MD">AWS Builder</p>
+        <p className="start-build-DNf">Start Build</p>
+        <p className="build-supplies-YA3">Build Supplies</p>
+        <p className="tooling-Tno">Tooling</p>
+        <p className="bluehosting-8tw">BlueHosting</p>
+        <p className="home-TgK">Home</p>
+        <p className="hosting-for-all-k9d">Hosting for all</p>
+        <p className="hosting-3eX">Hosting</p>
+        <p className="hosting6-mqR">Hosting6</p>
+        <p className="hosting5-JaT">Hosting5</p>
+        <p className="best-choice-DBd">Best Choice</p>
+        <p className="item-1-ieB">1</p>
+        <p className="builder-1-rEb">Builder 1</p>
+        <p className="wixpro-72-inch-responsive-website-builder-comprehensive-digital-platform-creation-tool-streamlined-design-interface-for-professional-websites-and-online-stores-black-blue-inb">
+          <span className="wixpro-72-inch-responsive-website-builder-comprehensive-digital-platform-creation-tool-streamlined-design-interface-for-professional-websites-and-online-stores-black-blue-inb-sub-0">WixPro 72-Inch Responsive Website Builder-</span>
+          <span className="wixpro-72-inch-responsive-website-builder-comprehensive-digital-platform-creation-tool-streamlined-design-interface-for-professional-websites-and-online-stores-black-blue-inb-sub-1">Comprehensive Digital Platform Creation Tool, Streamlined Design Interface for Professional Websites and Online Stores (Black/Blue)</span>
+        </p>
+        <p className="main-highlights-Uoh">Main highlights</p>
+        <p className="what-you-get-receive-the-wixpro-website-builder-suite-access-to-premium-design-templates-an-extensive-library-of-widgets-and-apps-and-detailed-step-by-step-guides-bdR">[What You Get]: Receive the WixPro website builder suite, access to premium design templates, an extensive library of widgets and apps, and detailed step-by-step guides.</p>
+        <img className="download-1-dKD" src="./assets/download-1.png" />
+        <p className="show-more-wao">Show more</p>
+        <p className="item-98-SXZ">9.8</p>
+        <p className="exceptional-MuR">Exceptional</p>
+        <p className="view-66K">View</p>
+        <p className="best-value-aXH">Best Value</p>
+        <p className="item-2-t2B">2</p>
+        <p className="biulder-oew">Biulder</p>
+        <p className="sitecraft-68-inch-ultimate-web-design-studio-advanced-site-creation-toolkit-intuitive-drag-and-drop-editor-for-dynamic-websites-and-e-commerce-platforms-green-white-68F">
+          <span className="sitecraft-68-inch-ultimate-web-design-studio-advanced-site-creation-toolkit-intuitive-drag-and-drop-editor-for-dynamic-websites-and-e-commerce-platforms-green-white-68F-sub-0">SiteCraft 68-Inch Ultimate Web Design Studio-</span>
+          <span className="sitecraft-68-inch-ultimate-web-design-studio-advanced-site-creation-toolkit-intuitive-drag-and-drop-editor-for-dynamic-websites-and-e-commerce-platforms-green-white-68F-sub-1"> Advanced Site Creation Toolkit, Intuitive Drag-and-Drop Editor for Dynamic Websites and E-commerce Platforms (Green/White)</span>
+        </p>
+        <p className="main-highlights-WbH">Main highlights</p>
+        <p className="what-you-get-gain-access-to-the-sitecraft-design-studio-featuring-a-robust-selection-of-design-elements-seo-optimization-tools-and-e-commerce-integrations-DEo">[What You Get]: Gain access to the SiteCraft design studio, featuring a robust selection of design elements, SEO optimization tools, and e-commerce integrations.</p>
+        <p className="show-more-Gis">Show more</p>
+        <p className="item-95-A3Z">9.5</p>
+        <p className="excellent-3d9">Excellent</p>
+        <p className="view-yFu">
+          View
+          <br />
+        </p>
+        <p className="related-deals-you-might-like-for-hSo">Related deals you might like for</p>
+        <p className="sign-up-and-get-exclusive-special-deals-cZm">Sign up and get exclusive special deals</p>
+        <p className="sign-up-8Y7">Sign Up</p>
+        <p className="categories-emM">Categories</p>
+        <p className="web-builder-xX9">Web Builder</p>
+        <p className="hosting-fgT">Hosting</p>
+        <p className="data-center-khu">Data Center</p>
+        <p className="robotic-automation-tZD">Robotic-Automation</p>
+        <p className="contact-AFq">Contact</p>
+        <p className="contact-sg3">Contact</p>
+        <p className="privacy-policy-c7q">Privacy Policy</p>
+        <p className="terms-of-service-51R">Terms Of Service</p>
+        <p className="categories-XPD">Categories</p>
+        <p className="about-E2j">About</p>
+        <p className="united-states-xDd">United States</p>
+        <img className="vector-srP" src="./assets/vector-ygs.png" />
+        <img className="vector-CNs" src="./assets/vector-Fyq.png" />
+        <img className="vector-XR9" src="./assets/vector-k8w.png" />
+        <img className="vector-ekf" src="./assets/vector-Ca3.png" />
+        <div className="rectangle-oNf">
+        </div>
+        <div className="rectangle-jXD">
+        </div>
+        <div className="rectangle-GX9">
+        </div>
+        <div className="rectangle-kx7">
+        </div>
+        <div className="rectangle-gas">
+        </div>
+        <img className="image-RoM" src="./assets/image-9gT.png" />
+        <img className="image-MS7" src="./assets/image-AmD.png" />
+        <img className="image-G3H" src="./assets/image-3Ao.png" />
+        <div className="rectangle-xwh">
+        </div>
+        <div className="image-tqM">
+          <img className="vector-aiB" src="./assets/vector-jBu.png" />
+          <img className="vector-LBZ" src="./assets/vector.png" />
+          <img className="vector-rvb" src="./assets/vector-N2B.png" />
+          <img className="vector-ces" src="./assets/vector-PBM.png" />
+          <img className="vector-AwH" src="./assets/vector-ECF.png" />
+        </div>
+        <div className="rectangle-Jnb">
+        </div>
+        <p className="cdk-resposive-builder-an-extensive-library-of-widgets-and-apps-and-detailed-step-by-step-guides-EAT">
+          <span className="cdk-resposive-builder-an-extensive-library-of-widgets-and-apps-and-detailed-step-by-step-guides-EAT-sub-0">CDK Resposive Builder:</span>
+          <span className="cdk-resposive-builder-an-extensive-library-of-widgets-and-apps-and-detailed-step-by-step-guides-EAT-sub-1"> An extensive library of widgets and apps, and detailed step-by-step guides</span>
+        </p>
+        <p className="off-5qH">26% Off</p>
+        <p className="item-99-1U3">9.9</p>
+        <p className="building-responsive-3fd">building responsive</p>
+        <p className="item-89-uxj">8.9</p>
+        <p className="cool-dtj">Cool</p>
+        <p className="item-89-wPd">8.9</p>
+        <p className="docs-faX">Docs</p>
+        <p className="why-we-love-it-aBh">Why we love it</p>
+        <p className="documentation-VpT">Documentation</p>
+        <p className="easy-use-xi3">Easy Use</p>
+        <p className="out-of-box-t5u">Out of box</p>
+        <p className="show-more-oif">Show more</p>
+        <p className="very-good-Wsy">Very Good</p>
+        <p className="view-E3H">View</p>
+        <p className="item-91-Xo5">9.1</p>
+        <img className="download-2-JhM" src="./assets/download-2.png" />
+        <img className="download-3-SYf" src="./assets/download-3.png" />
+        <div className="rectangle-xX1">
+        </div>
+        <div className="rectangle-tvT">
+        </div>
+        <div className="rectangle-32f">
+        </div>
+        <div className="rectangle-CZm">
+        </div>
+        <p className="off-wGT">20% Off</p>
+        <p className="limited-time-DDy">Limited time </p>
+        <p className="webbuilder-1-8rj">Webbuilder 1</p>
+        <p className="item-3996-1fd">$39.96</p>
+        <p className="item-4996-LC7">$49.96</p>
+        <p className="off-RUT">(20% Off)</p>
+        <p className="view-deal-vg7">View Deal</p>
+        <p className="computer-modern-clasic-with-wix-support-dqR">Computer  Modern clasic with wix support</p>
+        <img className="download-4-hqH" src="./assets/download-4.png" />
+        <div className="rectangle-Yb1">
+        </div>
+        <div className="rectangle-43Z">
+        </div>
+        <div className="rectangle-Bts">
+        </div>
+        <div className="rectangle-83R">
+        </div>
+        <p className="off-G9d">20% Off</p>
+        <p className="limited-time-n7y">Limited time </p>
+        <p className="webbuilder-1-tgo">Webbuilder 1</p>
+        <p className="item-3996-xwZ">$39.96</p>
+        <p className="item-4996-rn3">$49.96</p>
+        <p className="off-mu1">(20% Off)</p>
+        <p className="view-deal-JP9">View Deal</p>
+        <p className="computer-modern-clasic-with-wix-support-kkw">Computer  Modern clasic with wix support</p>
+        <img className="download-5-52X" src="./assets/download-5.png" />
+        <div className="rectangle-ojD">
+        </div>
+        <p className="item-4-h3u">4</p>
+        <img className="download-7-cAs" src="./assets/download-7.png" />
+        <p className="main-highlights-k2B">Main highlights</p>
+      </div>
+      <img className="image-p23" src="./assets/image-Gas.png" />
+      <div className="image-6kF">
+      </div>
+    </div>
+  </div>
+  );
+}
+
+export default App;
